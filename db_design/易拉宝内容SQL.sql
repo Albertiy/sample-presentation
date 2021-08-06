47ab909b25847a3c21e71ce0e891e48b6a32b185
Albertiy <2409205116@qq.com>
2021-08-06 17:24:16 +0800

    [2021-08-06 17:23:46] first commit

---
+++ db_design/易拉宝内容SQL.sql
@@ -0,0 +1,10 @@
+select * from product;
+
+insert into product(name) values('易拉宝');
+
+select * from category;
+
+insert into category(name) values('招牌');
+insert into category(name) values('美食');
+insert into category(name) values('培训');
+
